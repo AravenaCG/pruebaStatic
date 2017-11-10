3d639fc88bcf9e5115d4be5017e6064bfd516a2f
alumno <alumno@lab6pc0>
2017-11-09 21:03:09 -0300

    ABM done

---
+++ Practica_MVC_vGTK/Linux_64_C/Controlador.h
@@ -0,0 +1,15 @@
+
+
+void cont_init();
+
+int cont_altaSocio (char* nombre,char* apellido,char* dni);
+
+int cont_bajaSocio (int);
+
+int cont_modificarSocio (char* nombre,char* apellido,char* dni, int id);
+
+int cont_listarSocios ();
+
+void* cont_findSocio (int id);
+
+
